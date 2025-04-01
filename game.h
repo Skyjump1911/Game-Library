c1bb3c6d65a5f65e46da61af5fe3bc769650298e
Noah Melton <nmelton3@ut.utm.edu>
2025-03-31 19:46:45 -0500

    making the first steps

---
+++ game.h
@@ -0,0 +1,43 @@
+/**
+ * @file game.h
+ * @author Noah Melton
+ * @date 2025-03-31
+ * @brief This is the .h file for the game class. 
+ * 
+ * This is where the functions for the game class are created, but not implemented. 
+ */
+
+
+#ifndef GAME_H
+#define GAME_H
+
+
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <list>
+#include <iomanip>
+
+struct game {
+
+ public:
+  
+  string title; 
+  string publisher;
+  string genre;
+  float hours_played;
+  float price;
+  int year;
+  
+ private: 
+
+
+
+
+}
+
+
+
+
+
+#endif
---
+++ library.h
@@ -0,0 +1,67 @@
+/**
+ * @file library.h
+ * @author Noah Melton
+ * @date 2025-03-31
+ * @brief This is the .h file for the library class, where we define but don't provide implementation for the library class's functions. 
+ * 
+ * I worked on this alongside Sam Toney
+ */
+
+
+#include "game.h"
+
+#ifndef LIBRARY_H
+#define LIBRARY_H
+
+#include <string>
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <list>
+
+
+class library {
+
+ public:
+
+  //constructor
+  library();
+
+  //destructor
+  ~library();
+
+
+  //methods
+  void push_front(std::string title, std::string publisher, std::string genre, float hours_played, float price, int year);
+
+  void push_back(std::string title, std::string publisher, std::string genre, 
+float hours_played, float price, int year);      
+
+  void write_to_file(const std::string& filename);
+
+  void read_from_file(const std::string& filename); 
+
+  void insert_sorted(const std::string& title);
+
+  void find_genre(const std::string& genre);
+
+  void find_game(const std::string& title);
+
+  void delete(std::string title, int year);
+
+  void print(); 
+  
+
+
+}
+
+
+
+
+
+
+
+
+
+
+#ifndef
